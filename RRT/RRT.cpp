b1f547986983f9470c04db4a534d1218d013cb0c
joshlevin88 <joshua.levin@mail.mcgill.ca>
2017-09-01 12:08:39 -0400

    Wrote trim_end_states function and started writing main

--- RRT/RRT.cpp
+++ RRT/RRT.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<math.h>
+#include<time.h>
+#define PI 3.14159265358979323846f
 
+// Tree data structure
 struct node{
 	float coord[3];
 	float hdg;
-	int tr;
+	int tr_deg;
 	int zr;
 	int type;
 	float time;
@@ -14,14 +18,27 @@ struct node{
 	struct node* child;
 	struct node* parent;
 };
-
 typedef struct node node;
 
+// Constants
+const float V = 5.0f;
+const float t_iter = 0.5f;
+const float bf = 1.5f;
+const float t_int = 0.5f;
+const int max_tr_deg = 50;
+const int maz_zr = 1;
+const float coll_dist = 0.2f;
+const float ATA_dist = 9.0f;
+const float td = 0.23f;
+const float dt_max = 1.0f;
+const int near_max = 5;
+
+// Functions
 node* new_node(float, float, float, float, int, int, int, float, float, node*);
-node* new_rand_node(int);
+node* rand_node(int);
 node* add_sibling(node*, float, float, float, float, int, int, int, float, float);
 node* add_child(node*, float, float, float, float, int, int, int, float, float);
-
+void trim_end_states(node*, node*, int, int, float);
 /*
 node* search_tree(node*, int);
 node* compare_tree(node*);
@@ -32,28 +49,47 @@ void backwards(node*);
 int main()
 {
 	node* root = new_node(0, 0, 0, 0, 0, 0, 0, 0, 0, NULL);
-	add_child(root, 10, 0, 0, 0, 0, 0, 0, 2, 0);
+	node* second = add_child(root, 10, 0, 0, 0, 0, 0, 0, 2, 0);
+	printf("second - coord: [%.1f, %.1f, %.1f] - heading: %.1f \n", second->coord[0], second->coord[1], second->coord[2], second->hdg*180/PI);
+	trim_end_states(second, root, -40, 1, 10);
+	printf("second - coord: [%.1f, %.1f, %.1f] - heading: %.1f \n", second->coord[0], second->coord[1], second->coord[2], second->hdg*180/PI);
 
-	/*
-	int i;
-	node* root = new_node(NULL);
-	node* temp = root;
-	for (i = 1; i <= 12; i++){
-		if (i % 2 == 0) temp = add_child(temp);
-		else temp = add_sibling(temp);
+	bool path_found = false;
+	bool done = false;
+	int iter = 0;
+	node* commited_root = (node*)malloc(sizeof(node));
+	commited_root = root;
+	int gn = 1;
+	node* goal = new_node(45, 25, 5, 0, 0, 0, 0, 0, 0, NULL);
+
+	while (!done){
+
+		time_t start, end;
+		double elapsed;
+		start = time(NULL);
+		bool done_iter = false;
+
+		while (!done_iter){
+			end = time(NULL);
+			elapsed = difftime(end, start);
+			if (elapsed >= t_iter)
+				done_iter = true;
+		}
+		printf("Interval finished!");
 	}
-	*/
 }
 
-node* new_node(float coord_x, float coord_y, float coord_z, float hdg, int tr, int zr, int type, float time, float cost, node* parent)
+// Allocate dynamic memory and initialize new node
+node* new_node(float coord_x, float coord_y, float coord_z, float hdg, int tr_deg, int zr, int type, float time, float cost, node* parent)
 {
 	node* new_node = (node*) malloc(sizeof(node));
+
 	if (new_node) {
 		new_node->coord[0] = coord_x;
 		new_node->coord[1] = coord_y;
 		new_node->coord[2] = coord_z;
 		new_node->hdg = hdg;
-		new_node->tr = tr;
+		new_node->tr_deg = tr_deg;
 		new_node->zr = zr;
 		new_node->type = type;
 		new_node->time = time;
@@ -66,43 +102,69 @@ node* new_node(float coord_x, float coord_y, float coord_z, float hdg, int tr, i
 	return new_node;
 }
 
-node* new_rand_node(int iter)
+// Generate random node (revisit because this will create garbage)
+node* rand_node(int iter)
 {
-	node* new_rand_node = new_node(0, 0, 0, 0, 0, 0, 0, 0, 0, NULL);
+	node* rand_node = new_node(0, 0, 0, 0, 0, 0, 0, 0, 0, NULL);
 	int bias_freq = 3;
-	float goal_coord[3] = {50.0, 30.0, 10.0};
-	float x_max = 50.0;
-	float y_max = 30.0;
-	float z_max = 10.0;
+	float goal_coord[3] = {50.0f, 30.0f, 10.0f};
+	float x_max = 50.0f;
+	float y_max = 30.0f;
+	float z_max = 10.0f;
 
+	// Every bias_freq iterations, return goal node
 	if (iter % bias_freq == 0) {
-		new_rand_node->coord[0] = goal_coord[0];
-		new_rand_node->coord[1] = goal_coord[1];
-		new_rand_node->coord[2] = goal_coord[2];
+		rand_node->coord[0] = goal_coord[0];
+		rand_node->coord[1] = goal_coord[1];
+		rand_node->coord[2] = goal_coord[2];
 	}
 	else{
-		new_rand_node->coord[0] = ((float)rand() / (float)(RAND_MAX)) * x_max;
-		new_rand_node->coord[1] = ((float)rand() / (float)(RAND_MAX)) * y_max;
-		new_rand_node->coord[2] = ((float)rand() / (float)(RAND_MAX)) * z_max;
+		rand_node->coord[0] = ((float)rand() / (float)(RAND_MAX)) * x_max;
+		rand_node->coord[1] = ((float)rand() / (float)(RAND_MAX)) * y_max;
+		rand_node->coord[2] = ((float)rand() / (float)(RAND_MAX)) * z_max;
 	}
 
-	return new_rand_node;
+	return rand_node;
 }
 
-node* add_sibling(node* n, float coord_x, float coord_y, float coord_z, float hdg, int tr, int zr, int type, float time, float cost)
+// Add node to tree
+node* add_child(node* n, float coord_x, float coord_y, float coord_z, float hdg, int tr_deg, int zr, int type, float time, float cost)
+{
+	if (n == NULL) return NULL;
+	if (n->child) return add_sibling(n->child, coord_x, coord_y, coord_z, hdg, tr_deg, zr, type, time, cost);
+	else return (n->child = new_node(coord_x, coord_y, coord_z, hdg, tr_deg, zr, type, time, cost, n));
+}
+
+// Add node as sibling if parent node already has a child
+node* add_sibling(node* n, float coord_x, float coord_y, float coord_z, float hdg, int tr_deg, int zr, int type, float time, float cost)
 {
 	if (n == NULL) return NULL;
 
 	while (n->next) n = n->next;
 
-	return (n->next = new_node(coord_x, coord_y, coord_z, hdg, tr, zr, type, time, cost, n->parent));
+	return (n->next = new_node(coord_x, coord_y, coord_z, hdg, tr_deg, zr, type, time, cost, n->parent));
 }
 
-node* add_child(node* n, float coord_x, float coord_y, float coord_z, float hdg, int tr, int zr, int type, float time, float cost)
+// Calculate trim states (coordinates and heading) at end of trim primitive
+void trim_end_states(node* current, node* from, int tr_deg, int zr, float dt)
 {
-	if (n == NULL) return NULL;
-	if (n->child) return add_sibling(n->child, coord_x, coord_y, coord_z, hdg, tr, zr, type, time, cost);
-	else return (n->child = new_node(coord_x, coord_y, coord_z, hdg, tr, zr, type, time, cost, n));
+	float tr_rad = tr_deg*PI/180.0f;
+
+	if (tr_deg == 0){
+		current->coord[0] = from->coord[0] + V*dt*cos(from->hdg)*cos(asin(zr/V));
+		current->coord[1] = from->coord[1] + V*dt*sin(from->hdg)*cos(asin(zr / V));
+		current->coord[2] = from->coord[2] + zr*dt;
+	}
+	else{
+		current->coord[0] = from->coord[0] + (-V / tr_rad*sin(from->hdg - tr_rad*dt) + V / tr_rad*sin(from->hdg))*cos(asin(zr / V));
+		current->coord[1] = from->coord[1] + (V / tr_rad*cos(from->hdg - tr_rad*dt) - V / tr_rad*cos(from->hdg))*cos(asin(zr / V));
+		current->coord[2] = from->coord[2] + zr*dt;
+	}
+
+	current->hdg = from->hdg - tr_rad*dt;
+	current->hdg = fmod(current->hdg, 2 * PI);
+	if (current->hdg > PI) current->hdg = -2 * PI + current->hdg;
+	else if (current->hdg < -PI) current->hdg = 2 * PI + current->hdg; 
 }
 
 /*
